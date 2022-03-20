7ddd1f2ef499c21014593cf6967dd5cbb68b6a26
Oiseje Ojeikere <oiseje.ojeikere@kindredgroup.com>
2022-03-20 13:14:56 +0100

    feat: Add fetch abstraction

---
+++ src/common/services/data-service/data.service.ts
@@ -0,0 +1,26 @@
+export class DataService {
+  async get<Type>(url: string): Promise<Type> {
+    return fetch(url, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    }).then(this.processResponse);
+  }
+
+  async post<Type>(url: string, body: any): Promise<Type> {
+    return fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body)
+    }).then(this.processResponse);
+  }
+
+  private async processResponse(response: Response) {
+    if (response.ok) {
+      const resp = await response.json();
+      return resp.data;
+    } else {
+      const error = await response.json();
+      return Promise.reject(error);
+    }
+  }
+}
