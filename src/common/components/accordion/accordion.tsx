1d3d83ece85cdd3f1e201bae07f578cd68cabd2a
Oiseje Ojeikere <oiseje.ojeikere@kindredgroup.com>
2022-03-21 18:44:43 +0100

    feat: Add accordion for background selector

---
+++ src/common/components/accordion/accordion.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+import { AccordionItem } from './accordion-item';
+import { AccordionBody } from './accordion-body';
+import { AccordionHead } from './accordion-head';
+import { AccordionProvider } from './accordion-provider';
+
+const Accordion: FC<AccordionProps> = ({ children, toggle, defaultIndex, ...rest }) => {
+  return (
+    <AccordionProvider toggle={toggle} defaultIndex={defaultIndex}>
+      <div tabIndex={0} {...rest}>
+        {children}
+      </div>
+    </AccordionProvider>
+  );
+};
+
+Accordion.defaultProps = {
+  toggle: true
+};
+
+interface AccordionProps {
+  /**
+   *Default Accordion index(es)
+   */
+  defaultIndex?: number | number[];
+  /**
+   *
+   */
+  toggle?: boolean;
+}
+
+export default Object.assign(Accordion, { Item: AccordionItem, Head: AccordionHead, Body: AccordionBody });
--- src/create/background-selector/background-selector.tsx
+++ src/create/background-selector/background-selector.tsx
@@ -1,5 +1,22 @@
+import Accordion from '@common/components/accordion';
+
 const BackgroundSelector = () => {
-  return <div>Voice Selector</div>;
+  return (
+    <Accordion>
+      <Accordion.Item index={0}>
+        <Accordion.Head>Images</Accordion.Head>
+        <Accordion.Body>Image Content</Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item index={1}>
+        <Accordion.Head>Videos</Accordion.Head>
+        <Accordion.Body>Image Content</Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item index={2}>
+        <Accordion.Head>Colours</Accordion.Head>
+        <Accordion.Body>Colours Content</Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
 };
 
 export default BackgroundSelector;
