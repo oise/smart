5b2f23bb4a0040c875723b527b023c868e15dc18
Oiseje Ojeikere <oiseje.ojeikere@kindredgroup.com>
2022-03-22 11:14:10 +0100

    feat: Implement billing page

--- src/auth/signup/signup.tsx
+++ src/auth/signup/signup.tsx
@@ -7,27 +7,29 @@ const Signup = (): JSX.Element => {
   return (
     <Auth>
       <Header heading={'Signup'} showBorder={false} />
-      <div className='auth-container'>
-        <div className='auth-form'>
-          <form>
-            <InputField name={'fullName'} label={'Full name'} onChange={() => console.log('log')} placeholder={'Enter your fullname'} />
-            <InputField name={'emailAddress'} label={'Email Address'} onChange={() => console.log('log')} placeholder={'Enter your email'} />
-            <InputField
-              name={'password'}
-              label={'New Password'}
-              onChange={() => console.log('log')}
-              placeholder={'Enter your password'}
-              type='password'
-            />
-            <div className='auth-button'>
-              <button className='button button-primary' type={'button'}>
-                Sign up
-              </button>
-            </div>
-            <div className='auth-meta'>
-              Already a user? <NavLink to={'/'}> Login </NavLink>
-            </div>
-          </form>
+      <div className='layout-page-content'>
+        <div className='auth-container'>
+          <div className='auth-form'>
+            <form>
+              <InputField name={'fullName'} label={'Full name'} onChange={() => console.log('log')} placeholder={'Enter your fullname'} />
+              <InputField name={'emailAddress'} label={'Email Address'} onChange={() => console.log('log')} placeholder={'Enter your email'} />
+              <InputField
+                name={'password'}
+                label={'New Password'}
+                onChange={() => console.log('log')}
+                placeholder={'Enter your password'}
+                type='password'
+              />
+              <div className='auth-button'>
+                <button className='button button-primary' type={'button'}>
+                  Sign up
+                </button>
+              </div>
+              <div className='auth-meta'>
+                Already a user? <NavLink to={'/'}> Login </NavLink>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </Auth>
